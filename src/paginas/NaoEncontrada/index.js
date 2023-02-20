fec8497eef8dbbfe79d501873fb574c3790a9ae1
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2023-02-20 01:04:29 -0300

    Botoa GoTop

--- src/paginas/NaoEncontrada/index.js
+++ src/paginas/NaoEncontrada/index.js
@@ -9,7 +9,7 @@ function NaoEncontrada() {
       <div className={styles.conteudoContainer}>
         <span className={styles.texto404}>404</span>
 
-        <h1 className={styles.titulo}>Ops! Páginanão encontrada</h1>
+        <h1 className={styles.titulo}>Ops! Página não encontrada</h1>
 
         <p className={styles.paragrafo}>
           Tem certeza de que era isso que você estava procurando?
